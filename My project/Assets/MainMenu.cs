75bb4599e37b6ca21a577f88d5b18a98976a04c0
Alex <abelle2010@gmail.com>
2024-01-20 20:08:03 -0500

    Merged and linked Tinys page after menu

--- My project/Assets/MainMenu.cs
+++ My project/Assets/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("Game");
+        SceneManager.LoadScene("GamePlay");
 
         //Starts the next scene 
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);   
