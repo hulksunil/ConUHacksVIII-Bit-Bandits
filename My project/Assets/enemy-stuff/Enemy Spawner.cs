58852c5afe79cbb1061a2154c2df0571efbc6736
Sunil Kublalsingh <sunil@centrallshop.com>
2024-01-21 03:28:12 -0500

    changed spawn to limit each to only 3 zombies at a time. Also made it so that if they walked off the screen, they would disappear

--- My project/Assets/enemy-stuff/Enemy Spawner.cs
+++ My project/Assets/enemy-stuff/Enemy Spawner.cs
@@ -16,6 +16,14 @@ public class EnemySpawner : MonoBehaviour
 
     private float _timeUntilSpawn;
 
+    private List<UnityEngine.Object> _enemies;
+    private const int MAX_ENEMIES = 3;
+
+    void Start()
+    {
+        _enemies = new List<UnityEngine.Object>();
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -23,9 +31,28 @@ public class EnemySpawner : MonoBehaviour
 
         if (_timeUntilSpawn <= 0)
         {
-            Instantiate(_enemy, transform.position, Quaternion.identity);
+            // limit ememies to 3
+            if (_enemies.Count < MAX_ENEMIES)
+            {
+                // spawn enemy and add it to the list
+                _enemies.Add(Instantiate(_enemy, transform.position, Quaternion.identity));
+            }
+            // reset timer
             SetTimeUntilSpawn();
         }
+
+        // loop through enemies and destroy them if they are off screen
+        for (int i = 0; i < _enemies.Count; i++)
+        {
+            // if enemy's x coordinate is less than -12, destroy it
+            if (((GameObject)_enemies[i]).transform.position.x < -12)
+            {
+                Destroy(_enemies[i]);
+                _enemies.RemoveAt(i);
+            }
+
+        }
+
     }
 
     void Awake()
